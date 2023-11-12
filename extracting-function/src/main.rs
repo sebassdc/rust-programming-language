1417673552ea5cc70040555cdea75c5ccc3bbd76
Sebastian Hurtado <sebassdc@gmail.com>
2023-11-11 23:58:55 -0500

    Implemented generic on largest fn

--- extracting-function/src/main.rs
+++ extracting-function/src/main.rs
@@ -1,13 +1,22 @@
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
 
-    let mut largest = &number_list[0];
-
-    for number in &number_list {
+    for number in list {
         if number > largest {
             largest = number;
         }
     }
 
-    println!("The largest number is {}", largest);
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is result: {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
